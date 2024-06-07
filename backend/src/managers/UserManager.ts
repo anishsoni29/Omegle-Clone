c5365a0c52b4fe2fa57364df47dfc12e7ae689f8
Anish Soni <anish.21cse189@rtu.ac.in>
2024-06-07 10:35:19 +0400

    added the final codes

--- backend/src/managers/UserManager.ts
+++ backend/src/managers/UserManager.ts
@@ -1,16 +1,14 @@
 import { Socket } from "socket.io";
 import { RoomManager } from "./RoomManager";
 
-let GlOBAL_ROOM_ID = 1;
-
 export interface User {
   socket: Socket;
   name: string;
 }
 
 export class UserManager {
-  private users: User[] = [];
-  private queue: string[] = [];
+  private users: User[];
+  private queue: string[];
   private roomManager: RoomManager;
 
   constructor() {
@@ -25,38 +23,51 @@ export class UserManager {
       socket,
     });
     this.queue.push(socket.id);
-    socket.send("lobby");
+    socket.emit("lobby");
     this.clearQueue();
     this.initHandlers(socket);
   }
+
   removeUser(socketId: string) {
     const user = this.users.find((x) => x.socket.id === socketId);
+
     this.users = this.users.filter((x) => x.socket.id !== socketId);
     this.queue = this.queue.filter((x) => x === socketId);
   }
 
   clearQueue() {
+    console.log("inside clear queues");
+    console.log(this.queue.length);
     if (this.queue.length < 2) {
       return;
     }
-    const user1 = this.users.find((x) => x.socket.id === this.queue.pop());
-    const user2 = this.users.find((x) => x.socket.id === this.queue.pop());
+
+    const id1 = this.queue.pop();
+    const id2 = this.queue.pop();
+    console.log("id is " + id1 + " " + id2);
+    const user1 = this.users.find((x) => x.socket.id === id1);
+    const user2 = this.users.find((x) => x.socket.id === id2);
 
     if (!user1 || !user2) {
       return;
     }
+    console.log("creating roonm");
+
     const room = this.roomManager.createRoom(user1, user2);
     this.clearQueue();
   }
 
   initHandlers(socket: Socket) {
     socket.on("offer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
-      this.roomManager.onOffer(roomId, sdp);
+      this.roomManager.onOffer(roomId, sdp, socket.id);
     });
+
     socket.on("answer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
-      this.roomManager.onAnswer(roomId, sdp);
+      this.roomManager.onAnswer(roomId, sdp, socket.id);
+    });
+
+    socket.on("add-ice-candidate", ({ candidate, roomId, type }) => {
+      this.roomManager.onIceCandidates(roomId, socket.id, candidate, type);
     });
   }
 }
-
-//sdp = session description protocol
